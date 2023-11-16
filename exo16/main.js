3546f4a733543ff43c028a994b38a54872d8bb27
Matvei Tabelev <matvei.tabelev@gmail.com>
2023-11-16 11:56:24 +0100

    modifiction points du joueur

--- exo16/main.js
+++ exo16/main.js
@@ -8,63 +8,62 @@ Cela va s'exécuter autant de tour que la valeur nbr de tour.
 Celui qui arrive à zéro à perdu.
 Ella va afficher le nom du gagnant.  */
 
-
 class Personnage {
-    constructor(nom, force, defense, vie) {
-      this.nom = nom;
-      this.force = force;
-      this.defense = defense;
-      this.vie = vie;
-    }
-  
-    attaquer(attaquant) {
-        
-        let degats = this.force - attaquant.defense;
-        console.log(`${this.nom} attaque ${attaquant.nom} et lui inflige ${degats} points de dégâts.`);
-        this.vie -= Math.max(degats,0);
-      }
+  constructor(nom, force, defense, vie) {
+    this.nom = nom;
+    this.force = force;
+    this.defense = defense;
+    this.vie = vie;
   }
-  
-  class Combat {
-    constructor(nbrTour, joueur1, joueur2) {
-      this.nbrTour = nbrTour;
-      this.joueur1 = joueur1;
-      this.joueur2 = joueur2;
-      this.scoreJoueur1 = 0;
-      this.scoreJoueur2 = 0;
+
+  attaquer(attaquant) {
+    let degats = this.force - attaquant.defense;
+    console.log(
+      `${this.nom} attaque ${attaquant.nom} et lui inflige ${degats} points de dégâts.`
+    );
+    this.vie -= Math.max(degats, 0);
+  }
+}
+
+class Combat {
+  constructor(nbrTour, joueur1, joueur2) {
+    this.nbrTour = nbrTour;
+    this.joueur1 = joueur1;
+    this.joueur2 = joueur2;
+    this.scoreJoueur1 = 0;
+    this.scoreJoueur2 = 0;
+  }
+
+  lancerCombat() {
+    for (let i = 0; i < this.nbrTour; i++) {
+      // Attaque du joueur 1
+      console.log(`Tour ${i + 1} : ${this.joueur1.nom} attaque.`);
+      this.joueur1.attaquer(this.joueur2);
+
+      // Attaque du joueur 2
+      console.log(`Tour ${i + 1} : ${this.joueur2.nom} attaque.`);
+      this.joueur2.attaquer(this.joueur1);
+
+      // Vérification du gagnant
+      if (this.joueur1.vie <= 0) {
+        this.scoreJoueur2++;
+      } else if (this.joueur2.vie <= 0) {
+        this.scoreJoueur1++;
+      }
     }
-  
-    lancerCombat() {
-      for (let i = 0; i < this.nbrTour; i++) {
-        // Attaque du joueur 1
-        console.log(`Tour ${i + 1} : ${this.joueur1.nom} attaque.`);
-        this.joueur1.attaquer(this.joueur2);
-  
-        // Attaque du joueur 2
-        console.log(`Tour ${i + 1} : ${this.joueur2.nom} attaque.`);
-        this.joueur2.attaquer(this.joueur1);
-  
-        // Vérification du gagnant
-        if (this.joueur1.vie <= 0) {
-          this.scoreJoueur2++;
-        } else if (this.joueur2.vie <= 0) {
-          this.scoreJoueur1++;
-        }
-      }
-  
-      // Affichage du gagnant
-      if (this.scoreJoueur1 > this.scoreJoueur2) {
-        console.log("Le gagnant est " + this.joueur1.nom);
-      } else {
-        console.log("Le gagnant est " + this.joueur2.nom);
-      }
+
+    // Affichage du gagnant
+    if (this.scoreJoueur1 > this.scoreJoueur2) {
+      console.log("Le gagnant est " + this.joueur1.nom);
+    } else {
+      console.log("Le gagnant est " + this.joueur2.nom);
     }
   }
-  
-  const joueur1 = new Personnage("Jean", 10, 10, 100);
-  const joueur2 = new Personnage("Pierre", 5, 10, 100);
-  
-  const combat = new Combat(5, joueur1, joueur2);
-  
-  combat.lancerCombat();
-  
+}
+
+const joueur1 = new Personnage("Jean", 10, 8, 100);
+const joueur2 = new Personnage("Pierre", 15, 6, 100);
+
+const combat = new Combat(5, joueur1, joueur2);
+
+combat.lancerCombat();
